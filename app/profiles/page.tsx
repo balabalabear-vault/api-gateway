06968785423030774b4bb58f8e9c66a688a6288a
Jack Kwok <jack.kwok@concept4.net>
2024-02-16 18:05:49 +0800

    playing with layouts

---
+++ app/profiles/page.tsx
@@ -0,0 +1,12 @@
+import { lusitana } from "../ui/fonts";
+
+
+export default function Page() {
+  return (
+    <>
+      <h1 className={lusitana.className}>
+        My Profile Picture
+      </h1>
+    </>
+  )
+}
---
+++ app/profiles/videos/page.tsx
@@ -0,0 +1,12 @@
+import { lusitana } from "../..//ui/fonts";
+
+
+export default function Page() {
+  return (
+    <>
+      <h1 className={lusitana.className}>
+        My videos
+      </h1>
+    </>
+  )
+}
