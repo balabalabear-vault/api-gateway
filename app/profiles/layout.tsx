d19888f22120d65b5daf3c81313f7a8ce2dd3ecb
Jack Kwok <jack.kwok@concept4.net>
2024-02-22 13:10:53 +0800

    hide the side bar if too small

--- app/profiles/layout.tsx
+++ app/profiles/layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
       <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-        <div className="w-full flex-none md:w-64">
+        <div className="w-full flex-none hidden md:w-64">
           <div>xd</div>
         </div>
         <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
