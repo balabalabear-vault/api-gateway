3b80e5e2673a29a85f77b979a58995106a5003b3
Jack Kwok <jack.kwok@concept4.net>
2024-02-22 13:11:22 +0800

    added a GET route

--- app/profiles/videos/api/route.ts
+++ app/profiles/videos/api/route.ts
@@ -1,8 +1,15 @@
+export async function GET(request: Request) {
+  const res = await fetch('http://localhost:9001/api/v1/muscle-groups')
+  const muscleGroups = await res.json()
+  return Response.json({ data: muscleGroups })
+}
+
 export function POST(req, res) {
 
     // not needed in NextJS v12+
     // const body = JSON.parse(req.body);
     console.log(req.body)
+    return Response.json({ data: 'hello' })
   
     // the rest of your code
   }
