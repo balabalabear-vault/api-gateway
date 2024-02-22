b00c7264df8fa4a602cd08e4ae231aeb301245b1
Jack Kwok <jack.kwok@concept4.net>
2024-02-22 13:11:32 +0800

    trying to post video

--- app/profiles/videos/page.tsx
+++ app/profiles/videos/page.tsx
@@ -3,16 +3,31 @@
 import { Button, Input } from "@nextui-org/react";
 import { lusitana } from "../..//ui/fonts";
 import { ChangeEvent, MouseEvent, ReactNode, useEffect, useState, useTransition } from "react";
-import AliceCarousel from "react-alice-carousel";
-import 'react-alice-carousel/lib/alice-carousel.css';
-
+import { useQuery } from "@tanstack/react-query";
 
 type FileWithObjectUrl = File & { objectUrl: string };
 
+function getMuscleGroups () {
+  return fetch(`http://localhost:9001/api/v1/muscle-groups`)
+    .then((res) => res.json())
+}
+
+function postVideos (video: FormData) {
+  return fetch("http://localhost:9001/api/v1/media/videos", {
+    method: "POST",
+    body: video,
+  })
+    .then((res) => res.json())
+}
+
 export default function Page() {
   const [videos, setVideos] = useState<FileWithObjectUrl[]>([]);
-  const [videoElements, setVideoElements] = useState<ReactNode[]>([]);
   const [isPending, startTransition] = useTransition();
+
+  const { data, isLoading, isError, isSuccess } = useQuery<any, Error>({
+    queryKey: ['muscleGroups'],
+    queryFn: () => getMuscleGroups()
+  });
 
   const handleOnChange = (event: ChangeEvent<HTMLInputElement>): void => {
     event.stopPropagation();
@@ -20,14 +35,35 @@ export default function Page() {
     if (uploaded === null || uploaded.length === 0) return;
     else {
       startTransition(() => {
-        const files = Object.values(uploaded).map((file) => ({ ...file, objectUrl: URL.createObjectURL(file) }))
+        const files = Object.values(uploaded).map((file) => {
+          const fileWithUrl = file as FileWithObjectUrl;
+          fileWithUrl.objectUrl = URL.createObjectURL(file);
+          return fileWithUrl;
+        })
+        console.log(files)
         setVideos([...videos, ...files]);
       })
     }
   }
 
-  const handleOnClick = async (event: MouseEvent<HTMLElement>) => {
-    const res = await fetch('/profiles/videos/api', {
+  useEffect(() => {
+    if (videos.length === 0) return;
+    for (const video of videos) {
+      const { objectUrl, ...file } = video;
+      const formData= new FormData();
+      formData.append('video', file);
+      const response = await fetch("http://example.org/post", {
+        method: "POST",
+        body: formData,
+      });
+
+    }
+
+
+  }, [videos])
+
+  const handleOnClick = (event: MouseEvent<HTMLElement>) => {
+    const res = fetch('/profiles/videos/api', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -37,37 +73,47 @@ export default function Page() {
     console.log(res)
   }
 
-  useEffect(() => {
-    if (videos.length === 0) return;
-    startTransition(() => {
-      setVideoElements(videos.map((video, index) => (
-        <video key={index} width="300" controls src={video.objectUrl} />
-      )))
-    })
-  }, [videos]);
-
-  console.log(isPending)
-
   return (
     <>
       <h1 className={lusitana.className}> My videos </h1>
-      <input
-        type="file"
-        accept="video/*"
-        placeholder="Uploads"
-        onChange={handleOnChange}
-        multiple
-      />
-      {
-        <AliceCarousel mouseTracking items={videoElements} />
-      }
-      <Button
+      <div>
+        <label
+          className="border-solid border-2 rounded border-slate-300"
+          htmlFor="video_uploads"
+        >
+          Choose video to upload
+        </label>
+        <input
+          id="video_uploads"
+          name="video_uploads"
+          type="file"
+          accept="video/*"
+          multiple
+          style={{ opacity: 0 }}
+          onChange={handleOnChange}
+        />
+      </div>
+      <div className="flex flex-col">
+        {
+          videos.map((video, index) => (
+            <div className="grid grid-cols-3 justify-start items-center" key={index}>
+              <div>1</div>
+              <div className="col-span-2">
+                {video.name}
+            
+              </div>
+            </div>
+          ))
+        }
+      </div>
+
+      {/* <Button
         color="primary"
         isDisabled={videos.length === 0}
         onClick={handleOnClick}
       > 
         Upload
-      </Button>
+      </Button> */}
 
     </>
   )
