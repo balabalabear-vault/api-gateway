69882def90dd865ed336fc1eb65815957bfb2c66
Jack Kwok <jack.kwok@concept4.net>
2024-02-26 17:55:28 +0800

    showing upload progress

--- app/profiles/videos/VideoListing.tsx
+++ app/profiles/videos/VideoListing.tsx
@@ -2,7 +2,7 @@
 
 import BinIcon from "@/app/ui/icons/Bin";
 import DetailsIcon from "@/app/ui/icons/Details";
-import { Button } from "@nextui-org/react";
+import { Button, Progress } from "@nextui-org/react";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { ETargetVideoAction, TTargetVideoAction } from "./page";
 import { useMutation } from "@tanstack/react-query";
@@ -37,33 +37,42 @@ export default function VideoListing({
     },
   })
 
-useEffect(() => { console.log('hi'); postVideo.mutate(video) }, [])
+useEffect(() => { postVideo.mutate(video) }, [])
 
 	return (
-		<div className="grid grid-cols-4 justify-start items-center my-4">
-			<div>{progress}</div>
-			<div className="col-span-2">
-				{video.name}
-			</div>
-			<div className="justify-self-end">
-				<Button
-					isIconOnly
-					color="warning"
-					variant="faded"
-					onClick={() => setTargetVideoAction({id: tempId, action: ETargetVideoAction.DELETE})
-				}>
-					<BinIcon />
-				</Button>
-				<Button
-					isIconOnly
-					color="warning"
-					variant="faded"
-					onClick={() => setTargetVideoAction({id: tempId, action: ETargetVideoAction.EDIT})}
-				>
-					<DetailsIcon />
-				</Button>
-			</div>
-		</div>
+    <div className="grid grid-cols-4 justify-start items-center my-4 relative">
+      <div>{progress}</div>
+      <div className="col-span-2">
+        {video.name}
+      </div>
+      <div className="justify-self-end">
+        <Button
+          isIconOnly
+          color="warning"
+          variant="faded"
+          onClick={() => setTargetVideoAction({id: tempId, action: ETargetVideoAction.DELETE})
+        }>
+          <BinIcon />
+        </Button>
+        <Button
+          isIconOnly
+          color="warning"
+          variant="faded"
+          onClick={() => setTargetVideoAction({id: tempId, action: ETargetVideoAction.EDIT})}
+        >
+          <DetailsIcon />
+        </Button>
+      </div>
+      <div className="absolute bottom-0 w-full">
+        <Progress
+          isStriped
+          size="sm"
+          value={progress}
+          color="success"
+          className="max-w-md"
+        />
+      </div>
+    </div>
 	)
 
 }
