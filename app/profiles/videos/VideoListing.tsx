5a9689f349e91c2d3b72d1716bd26aa15237d506
Jack Kwok <jack.kwok@concept4.net>
2024-02-26 11:43:57 +0800

    showing upload progress

--- app/profiles/videos/VideoListing.tsx
+++ app/profiles/videos/VideoListing.tsx
@@ -3,9 +3,10 @@
 import BinIcon from "@/app/ui/icons/Bin";
 import DetailsIcon from "@/app/ui/icons/Details";
 import { Button } from "@nextui-org/react";
-import { Dispatch, SetStateAction, useEffect } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { ETargetVideoAction, TTargetVideoAction } from "./page";
 import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosProgressEvent } from "axios";
 
 export default function VideoListing({
 	tempId,
@@ -16,24 +17,31 @@ export default function VideoListing({
   video: File;
 	setTargetVideoAction: Dispatch<SetStateAction<null | TTargetVideoAction>>
 }>) {
+  const [progress, setProgress] = useState<number | undefined>(undefined);
 
 	const postVideo = useMutation<any, Error, File>({
     mutationFn: async (video: File) => {
 			const formData = new FormData();
 			formData.append('videos', video);
-      const res = await fetch("http://localhost:9001/api/v1/videos", {
-				method: "POST",
-				body: formData,
-			})
+      const res = await axios.postForm("http://localhost:9001/api/v1/videos", {
+          videos: video,
+        }, {
+          onUploadProgress: (progressEvent: AxiosProgressEvent) => (
+            setProgress(
+              progressEvent.progress ? Number((progressEvent.progress * 100).toPrecision(2)) : undefined
+            )
+          )
+        }
+      )
 			return res;
     },
   })
 
-	useEffect(() => { postVideo.mutate(video) }, [])
+useEffect(() => { console.log('hi'); postVideo.mutate(video) }, [])
 
 	return (
 		<div className="grid grid-cols-4 justify-start items-center my-4">
-			<div>1</div>
+			<div>{progress}</div>
 			<div className="col-span-2">
 				{video.name}
 			</div>
