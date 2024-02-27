bed324201f7e3e30c646e4f291282864ea81531f
Jack Kwok <jack.kwok@concept4.net>
2024-02-27 16:10:48 +0800

    temp work

--- app/profiles/videos/DatePicker.tsx
+++ app/profiles/videos/DatePicker.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Accordion, AccordionItem } from "@nextui-org/react";
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useRef, useState } from "react";
 import Calendar from "react-calendar";
 import { Value } from "./page";
 
@@ -11,11 +11,11 @@ export default function DatePicker({
   value: Value,
   onChange: Dispatch<SetStateAction<Value>>,
 }>) {
-  const [dummy, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
 
-
+  // server and client is returning different string
   const displayingDate = value?.toLocaleString().split(',')[0]
-    ?? new Date().toLocaleString().split(',')[0];
+    ?? "new Date().toISOString().split(',')[0]";
 
   const handleOnChange = (event: any) => {
     onChange(event);
@@ -28,10 +28,9 @@ export default function DatePicker({
       isCompact
     >
       <AccordionItem
-        key={"date"}
         aria-label={displayingDate}
         title={displayingDate}
-        className="text-center text-white"
+        className={`text-center text-white`}
         classNames={{
           title: "text-center",
           trigger: "color-red"
