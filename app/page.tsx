cfe8696288ddf26698c94056fb388e267b7a7459
Jack Kwok <jack.kwok@concept4.net>
2024-03-03 00:54:49 +0800

    added a endind section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
+'use client'
 import Concept4Section from "./components/Concept4Section";
 import LandingSection from "./components/LandingSection";
 import SelfIntroSection from "./components/SelfIntroSection";
+import EndingSection from "./components/EndingSection";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center">
+    <main className="flex flex-col items-center justify-center bg-slate-800">
       <LandingSection />
       <SelfIntroSection />
       <Concept4Section />
-      <section className="w-full min-h-lvh" />
-
+      <EndingSection />
     </main>
   );
 }
