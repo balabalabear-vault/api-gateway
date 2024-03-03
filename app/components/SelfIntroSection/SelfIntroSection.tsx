85d3785fd94c4f58c99ba3855496af44c3c1526d
Jack Kwok <jack.kwok@concept4.net>
2024-03-03 14:09:19 +0800

    display different text according to screen size

--- app/components/SelfIntroSection/SelfIntroSection.tsx
+++ app/components/SelfIntroSection/SelfIntroSection.tsx
@@ -3,9 +3,14 @@ import { lusitana } from "@/app/ui/fonts";
 import { motion, useTransform } from "framer-motion";
 import Image from "next/image";
 import { useScroll } from "framer-motion"
-import { useRef } from "react";
+import { useContext, useEffect, useMemo, useRef, useState } from "react";
+import { MediaContext } from "@/app/providers/Responsive/ResponsiveProvider";
 
 export default function SelfIntroSection() {
+  const media = useContext(MediaContext);
+  const [isDesktop, setIsDesktop] = useState(media.isDesktop);
+  useEffect(() => { if (media.isDesktop !== isDesktop) setIsDesktop(media.isDesktop); }, [media])
+  
   const sectionRef = useRef(null)
   const { scrollYProgress } = useScroll({
     target: sectionRef,
@@ -14,6 +19,22 @@ export default function SelfIntroSection() {
 
   const opacity = useTransform(scrollYProgress, [0, 0.8, 1], [0.2, 0.6, 1]);
   const y = useTransform(scrollYProgress, [0, 0.7], [200, 0]);
+
+  const detailText = useMemo(() => (
+    <>
+      <p className={`${lusitana.className} my-2 leading-7`}>I am Kwok Yui Kuen, Jack, a full stack developer from Hong Kong. I have a broad range of interests from astronomy to meditation. The world is full of excitement that I am eager to learn. Yet, coding will always be my prime enjoyment as the sense of satisfaction when I built something that can help people is irreplaceable.</p>
+      <p className={`${lusitana.className} my-2 leading-7`}>Sports is probably my second life.  I cannot live without it. Water sports and racket sports are all welcomed. Winter sports is something I want to master when I moved to Toronto xD</p>
+      <p className={`${lusitana.className} my-2 leading-7`}>I would love to share more about myself, but maybe you only want to take a fast grasp of my website. So ... Let's Scroll!</p>
+    </>
+  ), []);
+
+  const briefText = useMemo(() => (
+    <>
+      <p className={`${lusitana.className} my-2 leading-7 text-justify`}>I am Kwok Yui Kuen, Jack, a full stack developer from Hong Kong. The world is full of excitements but coding will always be my prime as building something that can help people is meaningful to me.</p>
+      <p className={`${lusitana.className} my-2 leading-7 text-justify`}>Sports is my second life.  I cannot live without it. Winter sports is something I want to master later on xD</p>
+      <p className={`${lusitana.className} my-2 leading-7 text-justify`}>I would love to share more about myself, but maybe you only want a fast grasp of my website. So ... Let's Scroll!</p>
+    </>
+  ), []);
 
   return (
     <motion.section
@@ -30,9 +51,7 @@ export default function SelfIntroSection() {
           />
         </div>
         <div className="col-span-2 text-white flex flex-col justify-around">
-          <p className={`${lusitana.className} my-2 leading-7`}>I am Kwok Yui Kuen, Jack, a full stack developer from Hong Kong. I have a broad range of interests from astronomy to meditation. The world is full of excitement that I am eager to learn. Yet, coding will always be my prime enjoyment as the sense of satisfaction when I built something that can help people is irreplaceable.</p>
-          <p className={`${lusitana.className} my-2 leading-7`}>Sports is probably my second life.  I cannot live without it. Water sports and racket sports are all welcomed. Winter sports is something I want to master when I moved to Toronto xD</p>
-          <p className={`${lusitana.className} my-2 leading-7`}>I would love to share more about myself, but maybe you only want to take a fast grasp of my website. So ... Let's Scroll!</p>
+          {isDesktop ? detailText : briefText}
         </div>
       </motion.div>
     </motion.section>
