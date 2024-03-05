bb8a41c1c83693da55d9be25fb99590f7d0b7d57
Jack Kwok <jack.kwok@concept4.net>
2024-03-05 15:37:11 +0800

    implemented socket io here

--- app/gym/components/MyVideos.tsx
+++ app/gym/components/MyVideos.tsx
@@ -2,8 +2,8 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { lusitana } from "@/app/ui/fonts";
-import { useEffect } from "react";
-import { io } from "socket.io-client";
+import { useCallback, useEffect, useState } from "react";
+import { Manager, Socket, io } from "socket.io-client";
 
 
 async function getMyVideos (date:string) {
@@ -23,31 +23,44 @@ export default function MyVideos({
     queryFn: () => getMyVideos(date)
   });
 
+  const [manager, setManager] = useState<Manager>();
+  const [socket, setSocket] = useState<Socket>();
+  
   useEffect(() => {
-    const socket = io("http://localhost:9001/api/v1");
-    console.log('hi')
+    if (manager) return;
+    console.log('.')
+    setManager(new Manager("http://localhost:9001/api/v1"))
+  }, [])
 
+  useEffect(() => {
+    if (!manager) return;
+    setSocket(manager.socket("/"));
+    return () => { if (socket) socket.disconnect(); };
+  }, [manager])
+
+  useEffect(() => {
+    if (!socket) return;
     socket.on("connect", () => {
-      console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+      console.log(socket.id);
     });
     
     socket.on("disconnect", () => {
-      console.log(socket.id); // undefined
+      console.log(socket.id);
     });
 
     socket.on("connect_error", () => {
       setTimeout(() => {
-        console.log("reconnecting")
         socket.connect();
-      }, 1000);
+      }, 1000)
     });
+  }, [socket])
 
-    return () => { socket.disconnect(); console.log('bye'); };
-  }, [])
+  const handleOnClick = () => socket.emit("ping", "hello", "world");
 
   return (
     <div>
       <h1 className={lusitana.className}> My Videos </h1>
+      <button onClick={handleOnClick}>Emit</button>
       {
         data === undefined || data.length === 0
         ? (<div>empty</div>)
