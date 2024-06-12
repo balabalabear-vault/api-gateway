8c7ab5d4596bb720b407b741c1c2fe908e6dc838
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-06-12 16:33:30 -0400

    text change in desktop version as well

--- app/components/SelfIntroSection/SelfIntroSection.tsx
+++ app/components/SelfIntroSection/SelfIntroSection.tsx
@@ -22,7 +22,7 @@ export default function SelfIntroSection() {
 
   const detailText = useMemo(() => (
     <>
-      <p className={`${lusitana.className} my-2 leading-7`}>I am Kwok Yui Kuen, Jack, a full stack developer from Hong Kong. I have a broad range of interests from astronomy to meditation. The world is full of excitement that I am eager to learn. Yet, coding will always be my prime enjoyment as the sense of satisfaction when I built something that can help people is irreplaceable.</p>
+      <p className={`${lusitana.className} my-2 leading-7`}>I am Yui Kuen Kwok, Jack, a full stack developer from Hong Kong. I have a broad range of interests from astronomy to meditation. The world is full of excitement that I am eager to learn. Yet, coding will always be my prime enjoyment as the sense of satisfaction when I built something that can help people is irreplaceable.</p>
       <p className={`${lusitana.className} my-2 leading-7`}>Sports is probably my second life.  I cannot live without it. Water sports and racket sports are all welcomed. Winter sports is something I want to master when I moved to Toronto xD</p>
       <p className={`${lusitana.className} my-2 leading-7`}>I would love to share more about myself, but maybe you only want to take a fast grasp of my website. So ... Let&apos;s Scroll!</p>
     </>
