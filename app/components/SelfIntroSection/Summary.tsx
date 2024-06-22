7f7f2059150ee0470897490398c24fe07bccb531
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-06-21 21:16:20 -0400

    added a new Summary component with new rotating words

---
+++ app/components/SelfIntroSection/Summary.tsx
@@ -0,0 +1,53 @@
+import { lusitana } from "@/app/ui/fonts"
+import { useAnimate } from "framer-motion"
+import { useEffect } from "react"
+
+export default function Summary() {
+  const [scope, animate] = useAnimate()
+
+  useEffect(() => {
+    void animate(
+      [
+        [scope.current, { y: '0%' }, { duration: 0 }],
+        [scope.current, { y: '-100%' }, { duration: 0.3, at: '+1.3' }],
+        [scope.current, { y: '-200%' }, { duration: 0.3, at: '+1.3' }],
+        [scope.current, { y: '-300%' }, { duration: 0.3, at: '+1.3' }],
+        [scope.current, { y: '-200%' }, { duration: 0.3, at: '+1.3' }],
+        [scope.current, { y: '-100%' }, { duration: 0.3, at: '+1.3' }],
+        [scope.current, { y: '0%' }, { duration: 0.3, at: '+1.3' }],
+      ],
+      {
+        repeat: Number.POSITIVE_INFINITY,
+      }
+    )
+  }, [animate, scope])
+
+  const descriptions = [
+    'agile',
+    'earnest',
+    'enthusiastic',
+    'independent',
+  ]
+
+  const colorGenerator = (num: number) => num % 2 === 0 ? 'from-[#ff1835] to-[#ffc900]' : 'from-[#0077ff] to-[#00e7df]'
+
+  return (
+    <h1 className="text-xl font-bold my-1 mb-1">
+      Hi, I am Jack, an{' '}
+      <span className={`inline-grid h-7 overflow-hidden ${lusitana.className} my-2 leading-7`}>
+        <div className="inline-grid h-7" ref={scope}>
+          {
+            descriptions.map((description, index) => (
+              <span
+                className={`bg-clip-text flex justify-center align-center text-transparent bg-gradient-to-r ${colorGenerator(index)}`}
+              >
+                {description}
+              </span>
+            ))
+          }
+        </div>
+      </span>
+      {' '}Full Stack Developer with 3 years of experience mainly working on Reactjs, Nodejs and AWS service .
+    </h1>
+  )
+}
