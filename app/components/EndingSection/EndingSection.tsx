395f32261026385f2cd5fa745d09c0969b8c43f4
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-06-21 21:16:36 -0400

    change after the rotating words implementation

--- app/components/EndingSection/EndingSection.tsx
+++ app/components/EndingSection/EndingSection.tsx
@@ -5,7 +5,7 @@ import ContactMeForm from "./ContactMeForm";
 export default function EndingSection() {
 
   return (
-    <section className="w-full h-lvh p-24 flex flex-col items-center justify-center text-neutral-400">
+    <section className="w-full sm:h-lvh p-24 flex flex-col items-center justify-center text-neutral-400">
         <h1 className={`${medieval.className} my-2 leading-7 text-center`}>That&apos;s It !</h1>
         <p className={`${lusitana.className}} text-center`}>The content is not much yet, but I promise as my career develops, I will fill it and share with you.</p>
         <p className={`${lusitana.className}} leading-7 text-center`}>I hope you still enjoy the website. Drop me a notes if you want :p</p>
@@ -15,7 +15,7 @@ export default function EndingSection() {
         </div>
         
         <div className={`my-4  ${lusitana.className} flex flex-col md:flex-row justify-center items-center sm:gap-2 md:gap-4`}>
-          <span>© 2024 Kwok Yui Kuen</span>
+          <span>© 2024 Jack Kwok</span>
           <Link
             href="https://www.linkedin.com/in/yui-kuen-kwok"
             target="_blank"
--- app/components/SelfIntroSection/SelfIntroSection.tsx
+++ app/components/SelfIntroSection/SelfIntroSection.tsx
@@ -1,10 +1,11 @@
 'use client'
 import { lusitana } from "@/app/ui/fonts";
-import { motion, useTransform } from "framer-motion";
+import { easeOut, motion, useAnimate, useTransform } from "framer-motion";
 import Image from "next/image";
 import { useScroll } from "framer-motion"
 import { useContext, useEffect, useMemo, useRef, useState } from "react";
 import { MediaContext } from "@/app/providers/Responsive/ResponsiveProvider";
+import Summary from "./Summary";
 
 export default function SelfIntroSection() {
   const media = useContext(MediaContext);
@@ -22,19 +23,16 @@ export default function SelfIntroSection() {
 
   const detailText = useMemo(() => (
     <>
-      <p className={`${lusitana.className} my-2 leading-7`}>I am Yui Kuen Kwok, Jack, a full stack developer from Hong Kong. I have a broad range of interests from astronomy to meditation. The world is full of excitement that I am eager to learn. Yet, coding will always be my prime enjoyment as the sense of satisfaction when I built something that can help people is irreplaceable.</p>
-      <p className={`${lusitana.className} my-2 leading-7`}>Sports is probably my second life.  I cannot live without it. Water sports and racket sports are all welcomed. Winter sports is something I want to master when I moved to Toronto xD</p>
-      <p className={`${lusitana.className} my-2 leading-7`}>I would love to share more about myself, but maybe you only want to take a fast grasp of my website. So ... Let&apos;s Scroll!</p>
+      <p className={`my-2 leading-7`}>I have a broad range of interests from astronomy to meditation. The world is full of excitement that I am eager to learn. Yet, coding will always be my prime enjoyment as the sense of satisfaction when I built something that can help people is irreplaceable.</p>
+      <p className={`my-2 leading-7`}>Sports is probably my second life. I cannot live without it. Water sports and racket sports are all welcomed. Winter sports is something I want to master when I moved to Toronto xD</p>
+      <p className={`my-2 leading-7`}>I would love to share more about myself, but maybe you only want to take a fast grasp of my website. So ... Let&apos;s Scroll!</p>
     </>
   ), []);
 
   const briefText = useMemo(() => (
-    <>
-      <p className={`${lusitana.className} my-2 leading-7 text-justify`}>I am Yui Kuen Kwok, Jack, a full stack developer from Hong Kong. The world is full of excitements but coding will always be my prime as building something that can help people is meaningful to me.</p>
-      <p className={`${lusitana.className} my-2 leading-7 text-justify`}>Sports is my second life.  I cannot live without it. Winter sports is something I want to master later on xD</p>
-      <p className={`${lusitana.className} my-2 leading-7 text-justify`}>I would love to share more about myself, but maybe you only want a fast grasp of my website. So ... Let&apos;s Scroll!</p>
-    </>
+      <p className={`my-2 leading-7 text-justify`}>I am sharing more in Desktop view, but maybe you only want a fast grasp of my website. So ... Let&apos;s Scroll!</p>
   ), []);
+
 
   return (
     <motion.section
@@ -48,9 +46,10 @@ export default function SelfIntroSection() {
             alt="Profil Picture"
             width={448}
             height={672}
-          />
+            />
         </div>
-        <div className="col-span-2 text-white flex flex-col justify-around">
+        <div className={`${lusitana.className} col-span-2 text-white`}>
+          <Summary />
           {isDesktop ? detailText : briefText}
         </div>
       </motion.div>
