575fe42ac1329a6f72d1a4364b566088ef901c32
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-07-03 18:56:35 -0400

    feat: new tab leading to guestbook

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -17,6 +17,10 @@ export default function NavBar() {
         {
             name: "Travel",
             url: "/travels",
+        },
+        {
+            name: "Guestbook",
+            url: "/guestbook",
         }
     ]), []);
 
