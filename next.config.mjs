6405780b1c9070f167850f8d0c3634ebaac15ebc
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-08-06 16:01:11 -0400

    fix: config for next Image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    images: {
+        remotePatterns: [{
+          protocol: 'https',
+          hostname: 'd3tzq0axgoep76.cloudfront.net',
+          port: '',
+          pathname: '/**'
+        }]
+    },
 };
 
 export default nextConfig;
