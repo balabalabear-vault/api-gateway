494c8fb2f7c88f43be6703e193afdec3271ec906
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-08-06 15:48:33 -0400

    fix: https

--- app/constants/assets.ts
+++ app/constants/assets.ts
@@ -1,5 +1,5 @@
 
-export const CLOUD_FRONT_URL = 'd3tzq0axgoep76.cloudfront.net';
+export const CLOUD_FRONT_URL = 'https://d3tzq0axgoep76.cloudfront.net';
 export const albedo = './earth-texture/Albedo.jpg';
 export const bump = './earth-texture/Bump.jpg';
 export const clouds = './earth-texture/Clouds.png'
